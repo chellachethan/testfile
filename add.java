90ed9ca8742c5c4104495ef27525633336e350e6
chellachethan <112315115+chellachethan@users.noreply.github.com>
2023-09-04 20:18:03 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,8 +1,14 @@
-public class JavaExample {
-  public static void main(String[] args) {
-    int num1 = 5, num2 = 15,sum;
-    sum = num1+num2;
-    System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
-  }
-}
+import java.util.Scanner;
 
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+              Scanner scanner = new Scanner(System.in);
+System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+      System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+              scanner.close();
+        double sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
